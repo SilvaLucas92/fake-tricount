8d01d49ab96e700513f726f0c5e9b85838f1316d
Lucas Silva <l.silva8692@gmail.com>
2023-06-15 17:35:04 -0300

    lat changes

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { ShowNotification } from "@/components/ShowNotification";
 import { Alert } from "@/types/types";
+import AnimateComponent from "@/components/AnimateComponent/AnimateComponent";
 
 const initialValues: { password: string; email: string } = {
   password: "",
@@ -20,7 +21,6 @@ const validationSchema = Yup.object({
     .email("Invalid email address")
     .required("Email is required"),
 });
-
 
 const Login = () => {
   const router = useRouter();
@@ -65,61 +65,63 @@ const Login = () => {
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <section className="h-screen flex justify-center items-center">
-        {" "}
-        <div className="w-full p-10 md:w-2/4  lg:w-1/4  md:mx-auto mx-5 flex flex-col  gap-5 bg-white border border-gray-200 rounded-lg shadow ">
-          <h2 className="text-2xl	text-gray-900">Welcome Back, Sign In!</h2>
-          <Input
-            type="text"
-            name="email"
-            label="Email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            error={
-              formik.touched.email && formik.errors.email
-                ? formik.errors.email
-                : ""
-            }
-          />
-          <Input
-            type="password"
-            name="password"
-            label="Password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            error={
-              formik.touched.password && formik.errors.password
-                ? formik.errors.password
-                : ""
-            }
-          />
-          <Button
-            loading={isLoading}
-            text="Submit"
-            variant="filled"
-            w="w-full"
-            margin={"mt-2"}
-          />
-          <p className="text-sm  text-gray-500">
-            Don’t have an account yet?{" "}
-            <Link
-              href="/register"
-              className="text-blue-600 hover:underline dark:text-blue-500"
-            >
-              Sign up
-            </Link>
-          </p>
-        </div>
-        {alert && (
-          <ShowNotification
-            type={alert?.type}
-            msg={alert?.msg}
-            setAlert={setAlert}
-          />
-        )}
-      </section>
-    </form>
+    <AnimateComponent>
+      <form onSubmit={formik.handleSubmit}>
+        <section className="h-screen flex justify-center items-center">
+          {" "}
+          <div className="w-full p-10 md:w-2/4  lg:w-1/4  md:mx-auto mx-5 flex flex-col  gap-5 bg-white border border-gray-200 rounded-lg shadow ">
+            <h2 className="text-2xl	text-gray-900">Welcome Back, Sign In!</h2>
+            <Input
+              type="text"
+              name="email"
+              label="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.email && formik.errors.email
+                  ? formik.errors.email
+                  : ""
+              }
+            />
+            <Input
+              type="password"
+              name="password"
+              label="Password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.password && formik.errors.password
+                  ? formik.errors.password
+                  : ""
+              }
+            />
+            <Button
+              loading={isLoading}
+              text="Submit"
+              variant="filled"
+              w="w-full"
+              margin={"mt-2"}
+            />
+            <p className="text-sm  text-gray-500">
+              Don’t have an account yet?{" "}
+              <Link
+                href="/register"
+                className="text-blue-600 hover:underline dark:text-blue-500"
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+          {alert && (
+            <ShowNotification
+              type={alert?.type}
+              msg={alert?.msg}
+              setAlert={setAlert}
+            />
+          )}
+        </section>
+      </form>
+    </AnimateComponent>
   );
 };
 
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { ShowNotification } from "@/components/ShowNotification";
 import { Alert } from "@/types/types";
+import AnimateComponent from "@/components/AnimateComponent/AnimateComponent";
 
 const initialValues: { password: string; email: string; name?: string } = {
   password: "",
@@ -56,73 +57,74 @@ const Register = () => {
   });
 
   return (
-    <form onSubmit={formik.handleSubmit} className="">
-      <section className="h-screen  flex justify-center items-center">
-        {" "}
-        <div className="w-full p-10 md:w-2/4  lg:w-1/4 md:mx-auto mx-5 flex flex-col  gap-5 bg-white border border-gray-200 rounded-lg shadow">
-          <h2 className="text-2xl	text-gray-900">Register</h2>
-          <Input
-            type="text"
-            name="name"
-            label="Name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            error={
-              formik.touched.name && formik.errors.name
-                ? formik.errors.name
-                : ""
-            }
-          />
-          <Input
-            type="text"
-            name="email"
-            label="Email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            error={
-              formik.touched.email && formik.errors.email
-                ? formik.errors.email
-                : ""
-            }
-          />
-          <Input
-            type="password"
-            name="password"
-            label="Password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            error={
-              formik.touched.password && formik.errors.password
-                ? formik.errors.password
-                : ""
-            }
-          />
-          <Button
-            loading={isLoading}
-            w="w-full"
-            text="Submit"
-            variant="filled"
-            margin={"my-2"}
-          />
-          <p className="text-sm  text-gray-500">
-            Already have an account?{" "}
-            <Link
-              href="/login"
-              className="text-blue-600 hover:underline dark:text-blue-500"
-            >
-              Login here
-            </Link>
-          </p>
-        </div>
-        {alert && (
-          <ShowNotification
-            type={alert?.type}
-            msg={alert?.msg}
-            setAlert={setAlert}
-          />
-        )}
-      </section>
-    </form>
+    <AnimateComponent>
+      <form onSubmit={formik.handleSubmit} className="">
+        <section className="h-screen  flex justify-center items-center">
+          <div className="w-full p-10 md:w-2/4  lg:w-1/4 md:mx-auto mx-5 flex flex-col  gap-5 bg-white border border-gray-200 rounded-lg shadow">
+            <h2 className="text-2xl	text-gray-900">Register</h2>
+            <Input
+              type="text"
+              name="name"
+              label="Name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.name && formik.errors.name
+                  ? formik.errors.name
+                  : ""
+              }
+            />
+            <Input
+              type="text"
+              name="email"
+              label="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.email && formik.errors.email
+                  ? formik.errors.email
+                  : ""
+              }
+            />
+            <Input
+              type="password"
+              name="password"
+              label="Password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.password && formik.errors.password
+                  ? formik.errors.password
+                  : ""
+              }
+            />
+            <Button
+              loading={isLoading}
+              w="w-full"
+              text="Submit"
+              variant="filled"
+              margin={"my-2"}
+            />
+            <p className="text-sm  text-gray-500">
+              Already have an account?{" "}
+              <Link
+                href="/login"
+                className="text-blue-600 hover:underline dark:text-blue-500"
+              >
+                Login here
+              </Link>
+            </p>
+          </div>
+          {alert && (
+            <ShowNotification
+              type={alert?.type}
+              msg={alert?.msg}
+              setAlert={setAlert}
+            />
+          )}
+        </section>
+      </form>
+    </AnimateComponent>
   );
 };
 
